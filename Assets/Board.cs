5c4ffd0d493b3bd8e05d569dda9fbe7c090f673a
CasterraDev <mdgreen155@yahoo.com>
2020-10-04 03:39:30 -0400

    Bugged out gun

--- Assets/Board.cs
+++ Assets/Board.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class Board : MonoBehaviour
 {
     public float spd = 3f;
+    public float shooterCoolDown = 1f;
     public Transform target;
     public GameObject bullet;
     public GameObject forward;
+    public GameObject origin;
 
     private Vector3 zAxis = new Vector3(0, 0, 1);
+    private float timer;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        timer = Time.time;
     }
 
     // Update is called once per frame
@@ -25,9 +28,14 @@ public class Board : MonoBehaviour
 
         transform.RotateAround(target.position, zAxis, spd * -h * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            //Instantiate(bullet,,Quaternion.identity);
+            if (timer < Time.time)
+            {
+                Debug.Log(origin.transform.position);
+                Instantiate(bullet, new Vector3(origin.transform.position.x - 5,origin.transform.position.y), origin.transform.rotation);
+                timer = Time.time + shooterCoolDown;
+            }
         }
     }
 }
