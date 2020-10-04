b9289b76161ea95c96c4b1f8f15aa5ddc5d39e05
CasterraDev <casterradev@gmail.com>
2020-10-04 03:39:30 -0400

    Bugged out gun

--- Assets/Ship.cs
+++ Assets/Ship.cs
@@ -25,7 +25,6 @@ public class Ship : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision.tag);
         if (collision.CompareTag("Bullet"))
         {
             Destroy(gameObject);
